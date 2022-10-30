b793de2d912f7bfd3415e3b9d72cf02cfddc1707
blind-stranger <sofyaryabchenko21@gmail.com>
2022-10-30 18:14:57 +0700

    change head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,17 @@ import "../styles/globals.scss"
 import type { AppProps } from "next/app"
 import HeaderComponent from "../components/HeaderComponent"
 import FooterComponent from "../components/FooterComponent"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Head>
+                <title>Awesome Keyboards</title>
+            </Head>
             <HeaderComponent />
             <Component {...pageProps} />
-            <FooterComponent/>
+            <FooterComponent />
         </>
     )
 }
